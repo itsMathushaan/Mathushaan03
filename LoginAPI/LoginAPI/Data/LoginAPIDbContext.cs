d1f14fc54a8e74ec994d9fc63100763306f7b2c7
Mathu26 <122962832+Mathu26@users.noreply.github.com>
2023-01-26 16:38:59 +0530

    API-LOGIN

---
+++ LoginAPI/LoginAPI/Data/LoginAPIDbContext.cs
@@ -0,0 +1,15 @@
+﻿using LoginAPI.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LoginAPI.Data
+{
+    public class LoginAPIDbContext : IdentityDbContext
+    {
+        public LoginAPIDbContext(DbContextOptions connectionString) : base(connectionString)
+        {
+        }
+
+        public DbSet<Login> Logins  { get; set; }
+    }
+}
---
+++ LoginAPI/LoginAPI/Models/Login.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.EntityFrameworkCore;
+
+namespace LoginAPI.Models
+{
+    [Table("Login")]
+    public class Login
+    {
+        [Key]
+        public int Id { get; set; }
+        [ForeignKey("Employee")]
+        /*[AllowNull]*/
+        public int EmployeeId { get; set; }
+        public string Username { get; set; }
+
+        [MinLength(8)]
+        public string Password { get; set; }
+    }
+}
